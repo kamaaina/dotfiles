85c4e5960b45ca420b990c09e79391512dff75c0
mike <mike808@gmail.com>
2021-05-22 10:53:36 +0900

    fish config

---
+++ config.fish
@@ -0,0 +1,12 @@
+# do not show a message when launching fish
+set fish_greeting
+
+# key binding; namely caps lock -> esc
+xmodmap ~/.Xmodmap
+
+# # turn on numlock
+/usr/bin/numlockx on
+
+alias ll="exa -l -g --icons"
+alias ls=exa
+alias vim=nvim
