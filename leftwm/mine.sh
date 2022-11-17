d8f77711ee2daf630d2e2c934cb95267fe6f1356
mike <mike808@gmail.com>
2022-11-18 07:16:05 +0900

    initial config for leftwm

---
+++ leftwm/mine.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# each theme has an up and down file which is a script to start/stop processes
+# add this script to the up script in the specified theme. for example
+# in .config/leftwm/themes/candy/up, add the following line
+# $HOME/.config/leftwm/mine.sh
+
+xset r rate 400 50
+
