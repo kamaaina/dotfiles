6e316eee271a1ed4213a7f7881c8c71fa9c8b8ef
mike <mike808@gmail.com>
2022-12-18 18:45:08 +0900

    updated keymaps

--- nvim/lua/cfg/keymaps.lua
+++ nvim/lua/cfg/keymaps.lua
@@ -7,3 +7,27 @@ map('n', '<leader>h', ':nohlsearch<CR>')
 map('n', '<leader>tc', ':tabclose<CR>')
 map('n', '<leader>tn', ':tabnew')
 --map('n', '<leader>to', ':tabonl<Space>')
+
+-- in visual mode, highlight lines and use J or K to move them up or down
+map('v', 'K', ":m '<-2<CR>gv=gv");
+map('v', 'J', ":m '>+1<CR>gv=gv");
+
+-- keep cursor at some position when merging lines
+map('n', 'J', 'mzJ`z')
+
+-- keep cursor in middle of screen when paging up or down
+map('n', '<C-d>', '<C-d>zz')
+map('n', '<C-u>', '<C-u>zz')
+
+-- make search term stay in middle
+map('n', 'n', 'nzzzv')
+map('n', 'N', 'Nzzzv')
+
+-- copy to system clipboard
+map('n', '<leader>y', '\"+y')
+map('v', '<leader>y', '\"+y')
+
+-- format buffer
+map('n', '<leader>f', function()
+    vim.lsp.buf.format()
+end)
