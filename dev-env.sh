9921993846a965f859c16f5d518694a8a20f7360
mike <mike808@gmail.com>
2023-04-16 19:45:43 +0900

    fix to start tmux with no highlighted windows

--- dev-env.sh
+++ dev-env.sh
@@ -4,8 +4,10 @@ SESSION=mike
 echo "creating session '${SESSION}'"
 
 tmux new-session -s $SESSION -d -n code -c ~/devel fish
+tmux set-option -g monitor-activity off
 tmux new-window -t $SESSION -d -n compile -c ~/devel fish
 tmux new-window -t $SESSION -d -n home -c ~/ fish
 tmux select-window -t code
-tmux -2 attach-session -t $SESSION
+tmux set-hook client-attached 'run -C -d 3 "set-option -g monitor-activity on"'
+tmux -2 attach-session -t $SESSION -d
 
