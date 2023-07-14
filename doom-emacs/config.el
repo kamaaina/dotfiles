f3630a5d3514d9260f983c080844aa66fa3f954e
mike <mike808@gmail.com>
2023-07-14 19:28:58 +0900

    do not prompt to exit/quit emacs

--- doom-emacs/config.el
+++ doom-emacs/config.el
@@ -95,3 +95,6 @@
   '(org-level-4 ((t (:inherit outline-4 :height 1.2))))
   '(org-level-5 ((t (:inherit outline-5 :height 1.1))))
 )
+
+;; disable the confirmation of exiting emacs
+(setq confirm-kill-emacs nil)
