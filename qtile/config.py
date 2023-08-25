9fc8ad267094c0d747badd33cce0eb6f2658a2ee
mike <mike808@gmail.com>
2023-08-26 06:43:48 +0900

    comment out layouts that i'll probably never use

--- qtile/config.py
+++ qtile/config.py
@@ -226,10 +226,10 @@ layouts = [
     layout.MonadTall(margin=8, border_width=2, border_focus="#44bb60", border_normal="#1d2330"),
     layout.MonadWide(margin=8, border_width=2, border_focus="#5e81ac", border_normal="#4c566a"),
     layout.Matrix(**layout_theme),
-    layout.Bsp(**layout_theme),
+    #layout.Bsp(**layout_theme),
     layout.Floating(**layout_theme),
     layout.RatioTile(**layout_theme),
-    layout.Max(**layout_theme)
+    #layout.Max(**layout_theme)
 ]
 
 # COLORS FOR THE BAR
