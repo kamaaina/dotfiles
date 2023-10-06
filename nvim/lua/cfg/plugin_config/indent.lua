eb75205063c270fafe2a3ad808cf4cb716f3eb44
mike <mike808@gmail.com>
2023-10-07 07:38:39 +0900

    update indent_blankline from v2 to v3

--- nvim/lua/cfg/plugin_config/indent.lua
+++ nvim/lua/cfg/plugin_config/indent.lua
@@ -1,5 +1,16 @@
--- See `:help indent_blankline.txt`
-require('indent_blankline').setup {
-   char = '┊',
-   show_trailing_blankline_indent = false,
-}
+-- See `:help ibl.commands`
+
+-- require('ibl').setup {
+--    char = '┊',
+--    show_trailing_blankline_indent = false,
+-- }
+
+require('ibl').setup({
+  enabled = true,
+  scope = {
+    enabled = false,
+  },
+  indent = {
+    char = '▏',
+  },
+})
