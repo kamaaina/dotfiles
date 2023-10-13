a6a8acaf02d145a42ef44e6dcd41f7db9cd3cc3c
mike <mike808@gmail.com>
2023-10-14 08:42:44 +0900

    updates for rust lsp

--- nvim/lua/cfg/plugin_config/rust_lsp.lua
+++ nvim/lua/cfg/plugin_config/rust_lsp.lua
@@ -54,33 +54,36 @@ require("rust-tools").setup(opts)
 -- See https://github.com/hrsh7th/nvim-cmp#basic-configuration
 local cmp = require("cmp")
 cmp.setup({
-   preselect = cmp.PreselectMode.None,
-   snippet = {
+    preselect = cmp.PreselectMode.None,
+    snippet = {
+      -- REQUIRED - you must specify a snippet engine
       expand = function(args)
-         vim.fn["vsnip#anonymous"](args.body)
+        -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+        require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+        -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+        -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
       end,
-   },
-   mapping = {
-      ["<C-p>"] = cmp.mapping.select_prev_item(),
-      ["<C-n>"] = cmp.mapping.select_next_item(),
-      -- Add tab support
+    },
+    window = {
+      completion = cmp.config.window.bordered(),
+      -- documentation = cmp.config.window.bordered(),
+    },
+    mapping = cmp.mapping.preset.insert({
       ["<S-Tab>"] = cmp.mapping.select_prev_item(),
       ["<Tab>"] = cmp.mapping.select_next_item(),
-      ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-      ["<C-f>"] = cmp.mapping.scroll_docs(4),
-      ["<C-Space>"] = cmp.mapping.complete(),
-      ["<C-e>"] = cmp.mapping.close(),
-      ["<CR>"] = cmp.mapping.confirm({
-         behavior = cmp.ConfirmBehavior.Insert,
-         select = true,
-      }),
-   },
-
-   -- Installed sources
-   sources = {
-      { name = "nvim_lsp" },
-      { name = "vsnip" },
-      { name = "path" },
-      { name = "buffer" },
-   },
-})
+      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+      ['<C-f>'] = cmp.mapping.scroll_docs(4),
+      ['<C-Space>'] = cmp.mapping.complete(),
+      ['<C-e>'] = cmp.mapping.abort(),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    }),
+    sources = cmp.config.sources({
+      { name = 'nvim_lsp' },
+      -- { name = 'vsnip' }, -- For vsnip users.
+      { name = 'luasnip' }, -- For luasnip users.
+      -- { name = 'ultisnips' }, -- For ultisnips users.
+      -- { name = 'snippy' }, -- For snippy users.
+    }, {
+      { name = 'buffer' },
+    })
+  })
