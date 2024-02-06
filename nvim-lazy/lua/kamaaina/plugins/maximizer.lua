29bda5152033665e5c92869b5ec40a6ec18570dd
mike <mike808@gmail.com>
2024-02-06 12:42:02 +0900

    added maximizer plugin

---
+++ nvim-lazy/lua/kamaaina/plugins/maximizer.lua
@@ -0,0 +1,9 @@
+return {
+	"szw/vim-maximizer",
+	"",
+	-- 9 year old plugin, but still works. don't have the function hooks, but
+	-- that is ok, we can still set the keymap to what we want it
+	keys = {
+		{ "<leader>sm", "<cmd>MaximizerToggle<CR>", desc = "Maximize/minimize a split" },
+	},
+}
