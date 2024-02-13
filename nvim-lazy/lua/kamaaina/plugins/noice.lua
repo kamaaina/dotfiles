51ab25d5d273eef7b66817459fe64bd2935f8b84
mike <mike808@gmail.com>
2024-02-14 07:53:54 +0900

    use cmd popup for searching and place command popup in middle of screen

--- nvim-lazy/lua/kamaaina/plugins/noice.lua
+++ nvim-lazy/lua/kamaaina/plugins/noice.lua
@@ -25,8 +25,8 @@ return {
 			},
 			-- you can enable a preset for easier configuration
 			presets = {
-				bottom_search = true, -- use a classic bottom cmdline for search
-				command_palette = true, -- position the cmdline and popupmenu together
+				-- bottom_search = true, -- use a classic bottom cmdline for search
+				-- command_palette = true, -- position the cmdline and popupmenu together
 				long_message_to_split = true, -- long messages will be sent to a split
 				inc_rename = false, -- enables an input dialog for inc-rename.nvim
 				lsp_doc_border = false, -- add a border to hover docs and signature help
@@ -36,5 +36,9 @@ return {
 		require("notify").setup({
 			background_colour = "#1a1b26",
 		})
+
+		-- set keymaps
+		local keymap = vim.keymap -- for conciseness
+		keymap.set("n", "<leader>nd", "<cmd>Noicedismiss<cr>", { desc = "Dismiss Noice Message" })
 	end,
 }
