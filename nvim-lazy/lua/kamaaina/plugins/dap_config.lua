9ccf4da35e8750efa69a3fc9ad8d61560a413191
mike <mike808@gmail.com>
2024-05-12 21:14:39 +0900

    rust-tools has been archived, used rustacean instead

---
+++ nvim-lazy/lua/kamaaina/plugins/dap_config.lua
@@ -0,0 +1,24 @@
+return {
+	"rcarriga/nvim-dap-ui",
+	dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
+
+	config = function()
+		require("dapui").setup({})
+
+		local dap, dapui = require("dap"), require("dapui")
+
+		dap.listeners.after.event_initialized["dapui_config"] = function()
+			dapui.open()
+		end
+		dap.listeners.before.event_terminated["dapui_config"] = function()
+			dapui.close()
+		end
+		dap.listeners.before.event_exited["dapui_config"] = function()
+			dapui.close()
+		end
+
+		vim.keymap.set("n", "<leader>dt", ":DapToggleBreakpoint<CR>")
+		vim.keymap.set("n", "<leader>dx", ":DapTerminate<CR>")
+		vim.keymap.set("n", "<leader>do", ":DapStepOver<CR>")
+	end,
+}
