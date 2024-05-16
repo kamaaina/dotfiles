4298c27a83771f14d38b0385888e44d59d5ca134
mike <mike808@gmail.com>
2024-05-17 02:44:43 +0900

    added plugin for multi cursors and added some filters for noice messages

---
+++ nvim-lazy/lua/kamaaina/plugins/multi_cursors.lua
@@ -0,0 +1,59 @@
+return {
+	"mg979/vim-visual-multi",
+	config = function()
+		-- create a column of cursors from visual mode
+		vim.keymap.set(
+			"n",
+			"<C-S-n>",
+			":call vm#commands#add_cursor_down(0, v:count1)<cr>",
+			{ noremap = true, silent = true }
+		)
+		vim.keymap.set(
+			"n",
+			"<leader>ma",
+			"<Plug>(VM-Select-All)<Tab>",
+			{ noremap = true, silent = true, desc = "Add Cursor For All Occurances" }
+		)
+		vim.keymap.set(
+			{ "n", "v" },
+			"<leader>md",
+			"<Plug>(VM-Add-Cursor-Down)",
+			{ noremap = true, silent = true, desc = "Add Cursor Down" }
+		)
+		vim.keymap.set(
+			{ "n", "v" },
+			"<leader>mm",
+			"<Plug>(VM-Find-Under)<Tab>",
+			{ noremap = true, silent = true, desc = "Start Multiple Cursors" }
+		)
+		vim.keymap.set(
+			{ "n", "v" },
+			"<leader>mo",
+			"<Plug>(VM-Toggle-Mappings)",
+			{ noremap = true, silent = true, desc = "Toggle Mappings" }
+		)
+		vim.keymap.set(
+			"n",
+			"<leader>mt",
+			"<Plug>(VM-Toggle-Multiline)",
+			{ noremap = true, silent = true, desc = "Toggle Multiline" }
+		)
+		vim.keymap.set("n", "<leader>mc", "<Plug>(VM-Add-Cursor-At-Pos)", { desc = "Add Cursor" })
+
+		vim.cmd([[
+    " all mappings disabled except <C-n> (<C-n> can't be remapped)
+    let g:VM_default_mappings = 0
+
+    " To change any mapping you must first initialize the variable:
+    let g:VM_maps = {}
+    let g:VM_maps['Find Under'] = "<C-n>"
+
+    " https://github.com/mg979/vim-visual-multi/issues/172
+    let g:VM_maps['I BS'] = "" " disable backspace mapping
+    let g:VM_maps['I CtrlC'] = "" " disable backspace mapping
+
+	 let g:VM_add_cursor_at_pos_no_mappings = 1
+
+]])
+	end,
+}
--- nvim-lazy/lua/kamaaina/plugins/noice.lua
+++ nvim-lazy/lua/kamaaina/plugins/noice.lua
@@ -15,6 +15,55 @@ return {
 
 	config = function()
 		require("noice").setup({
+			routes = {
+				{
+					filter = {
+						event = "msg_show",
+						kind = "",
+						find = "written",
+					},
+					opts = { skip = true },
+				},
+			},
+			commands = {
+				history = {
+					-- options for the message history that you get with `:Noice`
+					view = "split",
+					opts = { enter = true, format = "details" },
+					filter = {
+						any = {
+							{ event = "notify" },
+							{ error = true },
+							{ warning = true },
+							{ event = "msg_show", kind = { "" } },
+							{ event = "lsp", kind = "message" },
+						},
+					},
+				},
+				-- :Noice last
+				last = {
+					view = "popup",
+					opts = { enter = true, format = "details" },
+					filter = {
+						any = {
+							{ event = "notify" },
+							{ error = true },
+							{ warning = true },
+							{ event = "msg_show", kind = { "" } },
+							{ event = "lsp", kind = "message" },
+						},
+					},
+					filter_opts = { count = 1 },
+				},
+				-- :Noice errors
+				errors = {
+					-- options for the message history that you get with `:Noice`
+					view = "popup",
+					opts = { enter = true, format = "details" },
+					filter = { error = true },
+					filter_opts = { reverse = true },
+				},
+			},
 			lsp = {
 				-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
 				override = {
@@ -25,8 +74,8 @@ return {
 			},
 			-- you can enable a preset for easier configuration
 			presets = {
-				-- bottom_search = true, -- use a classic bottom cmdline for search
-				-- command_palette = true, -- position the cmdline and popupmenu together
+				bottom_search = true, -- use a classic bottom cmdline for search
+				--command_palette = true, -- position the cmdline and popupmenu together
 				long_message_to_split = true, -- long messages will be sent to a split
 				inc_rename = false, -- enables an input dialog for inc-rename.nvim
 				lsp_doc_border = false, -- add a border to hover docs and signature help
@@ -35,6 +84,18 @@ return {
 
 		require("notify").setup({
 			background_colour = "#1a1b26",
+		})
+
+		require("lualine").setup({
+			sections = {
+				lualine_x = {
+					{
+						require("noice").api.statusline.mode.get,
+						cond = require("noice").api.statusline.mode.has,
+						color = { fg = "#ff9e64" },
+					},
+				},
+			},
 		})
 
 		-- set keymaps
