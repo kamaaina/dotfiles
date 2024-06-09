b7e5ae654871871a7f3c79f9debb7e8bfd7ec0c0
mike <mike808@gmail.com>
2024-06-09 19:43:25 +0900

    added configuration for polybar in i3

---
+++ polybar/weather.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+# Surf to https://openweathermap.org/city
+
+
+import requests
+
+CITY = "FIXME"
+API_KEY = "FIXME"
+UNITS = "Metric"
+UNIT_KEY = "C"
+#UNIT_KEY = "F"
+LANG = "en"
+
+REQ = requests.get("http://api.openweathermap.org/data/2.5/weather?id={}&lang={}&appid={}&units={}".format(CITY, LANG,  API_KEY, UNITS))
+try:
+    # HTTP CODE = OK
+    if REQ.status_code == 200:
+        CURRENT = REQ.json()["weather"][0]["description"].capitalize()
+        TEMP = int(float(REQ.json()["main"]["temp"]))
+        print("{}, {} °{}".format(CURRENT, TEMP, UNIT_KEY))
+    else:
+        print("Error: BAD HTTP STATUS CODE " + str(REQ.status_code))
+except (ValueError, IOError):
+    print("Error: Unable print the data")
