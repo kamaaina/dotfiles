01c1b88c399ef5db27310538cd5696dd1c1c5b55
mike <mike808@gmail.com>
2024-06-23 10:56:29 +0900

    starting transition to yazi

--- nvim/lua/kamaaina/plugins/nvim-tree.lua
+++ nvim/lua/kamaaina/plugins/nvim-tree.lua
@@ -53,7 +53,8 @@ return {
 		-- set keymaps
 		local keymap = vim.keymap -- for conciseness
 
-		keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" }) -- toggle file explorer
+		-- replacing with yazi
+		-- keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" }) -- toggle file explorer
 		keymap.set(
 			"n",
 			"<leader>ef",
