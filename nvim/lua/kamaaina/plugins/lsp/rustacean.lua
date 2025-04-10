a24909b3967a6fb0602e2a56bdb3705996babdb4
mike <mike808@gmail.com>
2025-04-10 19:22:55 +0900

    lsp changed to hide diagnostics by default, but i want them shown

--- nvim/lua/kamaaina/plugins/lsp/rustacean.lua
+++ nvim/lua/kamaaina/plugins/lsp/rustacean.lua
@@ -24,11 +24,20 @@ return {
 			server = {
 				on_attach = function(client, bufnr)
 					vim.api.nvim_set_hl(0, "LspInlayHint", { fg = "#5b6171", bg = "#1b1d22" })
+					vim.diagnostic.config({ virtual_text = true })
+					vim.lsp.buf.format({ async = true })
 					-- you can also put keymaps in here
+					-- vim.keymap.set("n", "<leader>df", "vim.diagnostic.open_float()", { desc = "floating diagnostic" })
+					-- vim.keymap.set("n", "<leader>do", function()
+					-- 	vim.diagnostic.setqflist({ open = true })
+					-- end, { desc = "Show diagnostics" })
 				end,
 				default_settings = {
 					-- rust-analyzer language server configuration
-					["rust-analyzer"] = {},
+					["rust-analyzer"] = {
+						check = { command = "clippy" },
+						checkOnSave = true,
+					},
 				},
 			},
 			-- DAP configuration
