ab8ba4bac3c4bfff1d26522c5b02903ed7241cd3
mike <mike808@gmail.com>
2025-05-05 07:44:35 +0900

    remove old obe plugin and fix the issue where buffere was being open with change flag set

--- nvim/lua/kamaaina/plugins/lsp/rustacean.lua
+++ nvim/lua/kamaaina/plugins/lsp/rustacean.lua
@@ -25,7 +25,7 @@ return {
 				on_attach = function(client, bufnr)
 					vim.api.nvim_set_hl(0, "LspInlayHint", { fg = "#5b6171", bg = "#1b1d22" })
 					vim.diagnostic.config({ virtual_text = true })
-					vim.lsp.buf.format({ async = true })
+					--vim.lsp.buf.format({ async = true })
 					-- you can also put keymaps in here
 					-- vim.keymap.set("n", "<leader>df", "vim.diagnostic.open_float()", { desc = "floating diagnostic" })
 					-- vim.keymap.set("n", "<leader>do", function()
