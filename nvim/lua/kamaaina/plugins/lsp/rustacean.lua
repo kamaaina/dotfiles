7b2771e12d776a1777384fd32f46dfd6bf7771dc
mike <mike808@gmail.com>
2026-02-21 20:06:37 +0900

    use latest and greatest for rustaceanvim lsp

--- nvim/lua/kamaaina/plugins/lsp/rustacean.lua
+++ nvim/lua/kamaaina/plugins/lsp/rustacean.lua
@@ -1,6 +1,6 @@
 return {
 	"mrcjkb/rustaceanvim",
-	version = "^5", -- Recommended
+	version = "^8", -- Recommended
 	lazy = false, -- This plugin is already lazy
 	vim.lsp.inlay_hint.enable(true),
 	config = function()
